80b629a5ad502d23a971f3a37c004f8a46350767
joacogru <joaquingru@gmail.com>
2022-08-02 20:43:35 +0300

    *algoritmo simple

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let cliente = prompt("Ingrese su nombre")
+let categoria = parseInt(prompt("Hola " + cliente + ", Ingrese su fecha de nacimiento:"))
+
+let edad = 2022 - categoria
+
+alert("Usted terminara este año con "+edad+" años")
+
